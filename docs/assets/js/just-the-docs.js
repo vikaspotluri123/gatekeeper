9863eb344c59990c42069a02b27af152c349d649
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-03-30 14:09:03 -0500

    docs: target modern browsers

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -1,20 +1,10 @@
-// Event handling
-
-function addEvent(el, type, handler) {
-    if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
-}
-function removeEvent(el, type, handler) {
-    if (el.detachEvent) el.detachEvent('on'+type, handler); else el.removeEventListener(type, handler);
-}
-
 // Show/hide mobile menu
-
-function toggleNav(){
+function toggleNav() {
   const nav = document.querySelector('.js-main-nav');
   const auxNav = document.querySelector('.js-aux-nav');
   const navTrigger = document.querySelector('.js-main-nav-trigger');
 
-  addEvent(navTrigger, 'click', function(){
+  navTrigger.addEventListener('click', function() {
     var text = navTrigger.innerText;
     var textToggle = navTrigger.getAttribute('data-text-toggle');
 
@@ -24,7 +14,7 @@ function toggleNav(){
     navTrigger.innerText = textToggle;
     navTrigger.setAttribute('data-text-toggle', text);
     textToggle = text;
-  })
+  });
 }
 
 function pageFocus() {
@@ -34,16 +24,13 @@ function pageFocus() {
 }
 
 // Document ready
-function ready(){
+function ready() {
   toggleNav();
   pageFocus();
 }
 
-// in case the document is already rendered
-if (document.readyState!='loading') ready();
-// modern browsers
-else if (document.addEventListener) document.addEventListener('DOMContentLoaded', ready);
-// IE <= 8
-else document.attachEvent('onreadystatechange', function(){
-    if (document.readyState=='complete') ready();
-});
+if (document.readyState !== 'loading') {
+	ready();
+} else {
+	document.addEventListener('DOMContentLoaded', ready)
+}
